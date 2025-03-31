2a6ad51bc66f37e569fcdde7ec1b2dc35dbd9a88
Ismael <ismaelguedessilva@gmail.com>
2025-03-31 10:57:24 -0300

    primeira tabela

--- src/recados/entities/recados.entity.ts
+++ src/recados/entities/recados.entity.ts
@@ -1,8 +1,28 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
 export class Recado {
-  id: number;
+  @PrimaryGeneratedColumn()
+  id: number; // automatico sequencial 1 2 3 4 5 ...
+  
+  @Column({type: 'varchar', length: 255}) // sem valores dentro de Column vai passar como texto
   texto: string;
+
+  @Column({type: 'varchar', length: 50})
   de: string;
+  
+  @Column({type: 'varchar', length: 50})
   para: string;
+
+  @Column({default: false})
   lido: boolean;
+
+  @Column()
   data: Date;
+  
+  @CreateDateColumn()
+  createdAt?: Date;
+  
+  @UpdateDateColumn()
+  updatedAt?: Date;
 }
--- src/recados/recados.module.ts
+++ src/recados/recados.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RecadosController } from './recados.controller';
 import { RecadosService } from './recados.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Recado } from './entities/recados.entity';
 
 @Module({
   controllers: [RecadosController],
   providers: [RecadosService],
+  imports: [TypeOrmModule.forFeature([Recado])]
 })
-export class RecadosModule {}
+export class RecadosModule { }
